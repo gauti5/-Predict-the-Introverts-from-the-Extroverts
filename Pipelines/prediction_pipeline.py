d775aafb012d4650b706423c8802f9a63fdc6627
Sandeep <sndpgauti@gmail.com>
2025-09-15 22:21:00 +0530

    Prediction Pipeline Completed

--- Pipelines/prediction_pipeline.py
+++ Pipelines/prediction_pipeline.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-import pandas as pf
+import pandas as pd
 
 from src.logging import logging
 from src.exception import CustomException
@@ -29,3 +29,42 @@ class predict_pipeline:
         except Exception as e:
             logging.info("Error occured during the prediction pipeline")
             raise CustomException(e,sys)
+        
+    class CustomData:
+        def __init__(self,
+                     id:int,
+                     Time_spent_Alone:float,
+                     Stage_fear:object,
+                     Social_event_attendance:float,
+                     Going_outside:float,
+                     Drained_after_socializing:object,
+                     Friends_circle_size:float,
+                     Post_frequency:float):
+            self.id=id
+            self.Time_spent_Alone=Time_spent_Alone
+            self.Stage_fear=Stage_fear
+            self.Social_event_attendance=Social_event_attendance
+            self.Going_outside=Going_outside
+            self.Drained_after_socializing=Drained_after_socializing
+            self.Friends_circle_size=Friends_circle_size
+            self.Post_frequency=Post_frequency
+            
+        def get_data_as_dataframe(self):
+            try:
+                custom_data_input_dict={
+                    'id':[self.id],
+                    'Time_spent_Alone':[self.Time_spent_Alone],
+                    'Stage_fear':[self.Stage_fear],
+                    'Social_event_attendance':[self.Social_event_attendance],
+                    'Going_outside':[self.Going_outside],
+                    'Drained_after_socializing':[self.Drained_after_socializing],
+                    'Drained_after_socializing':[self.Drained_after_socializing],
+                    'Friends_circle_size':[self.Friends_circle_size],
+                    'Post_frequency':[self.Post_frequency]
+                }
+                
+                df=pd.DataFrame(custom_data_input_dict)
+                return df
+            except Exception as e:
+                logging.info("Error ocuured during the prediction pipeline!!!")
+                raise CustomException(e,sys)
